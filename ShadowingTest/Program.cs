15a3a3d698ac825f973480defa65a5cb6243766d
Varun <varunvg0011@gmail.com>
2023-07-18 00:54:26 +0530

    Adding Shadowing Demo

---
+++ ShadowingTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowingTest
+{
+
+    public class Patient
+    {
+        public string GetPatient()
+        {
+            return "Base Patient";
+        }
+    }
+
+    public class GoldPatient:Patient
+    {
+        public string GetPatient()
+        {
+            return "Child Patient";
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Patient patient = new Patient();
+            Console.WriteLine(patient.GetPatient());
+        }
+    }
+}
